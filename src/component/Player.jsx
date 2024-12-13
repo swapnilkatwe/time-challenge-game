8d56fbd6cb6e2393ffb2ed20a491879d742b014b
Swapnil Katwe <swapnil.katwe111@gmail.com>
2024-12-13 12:25:10 +0100

    Two way binding for input field

--- src/component/Player.jsx
+++ src/component/Player.jsx
@@ -1,10 +1,22 @@
+import { useState } from "react";
+
 export default function Player() {
+  const [enteredPlayerName, setEnteredPlayerName] = useState("");
+  const [isSubmitted, setIsSubmitted] = useState(false);
+  
+  function inputEventHandler(value) {
+      setEnteredPlayerName(value);
+  }
+
+  function buttonHandler() {
+    setIsSubmitted(true)
+  }
     return(
         <section id="player">
-        <h2>Welcome unknown entity</h2>
+        <h2>Welcome {isSubmitted ? enteredPlayerName : "unknown entity"}</h2>
         <p>
-          <input type="text" />
-          <button>Set Name</button>
+          <input type="text" onChange={(event)=> inputEventHandler(event.target.value)} value={enteredPlayerName}/>
+          <button onClick={buttonHandler}>Set Name</button>
         </p>
       </section>
     );
