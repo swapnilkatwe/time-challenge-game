55e007cbdde1913c5fa39f804f85252d993c60e5
Swapnil Katwe <swapnil.katwe111@gmail.com>
2024-12-15 16:13:30 +0100

    Result Modal updated with title and score and reset timer on close button.

--- src/component/ResultModal.jsx
+++ src/component/ResultModal.jsx
@@ -1,9 +1,11 @@
 /* eslint-disable react/prop-types */
 import { useRef, forwardRef, useImperativeHandle } from 'react';
 
-const ResultModal = forwardRef(function ResultModal({result, targetTime}, ref) {
+const ResultModal = forwardRef(function ResultModal({ targetTime, timeRemaining, onReset}, ref) {
   
   const dialogRef = useRef();
+  const formattedTimeRemaining = (timeRemaining / 1000).toFixed(2);
+  const score = Math.round((1 - (timeRemaining /(targetTime * 1000))) * 100 );
   useImperativeHandle(ref, () => {
       return {
         open() {
@@ -14,14 +16,15 @@ const ResultModal = forwardRef(function ResultModal({result, targetTime}, ref) {
 
   return (
     <dialog ref={dialogRef} className="result-modal">
-      <h2>You {result}!</h2>
+      {timeRemaining <= 0 && <h2>You Lost!</h2>}
+      {timeRemaining > 0 && <h2>Your Score: {score}</h2>}
       <p>
         Your target time was <strong>{targetTime} Seconds.</strong>
       </p>
       <p>
-        You stop the timer with <strong>X seconds left</strong>
+        You stop the timer with <strong>{formattedTimeRemaining} seconds left</strong>
       </p>
-      <form method="dialog">
+      <form method="dialog" onSubmit={onReset}>
         <button>Close</button>
       </form>
     </dialog>
--- src/component/TimerChallenge.jsx
+++ src/component/TimerChallenge.jsx
@@ -4,38 +4,52 @@ import { useState, useRef } from "react";
 import ResultModal from "./ResultModal";
 
 export default function TimerChallenge({ title, targetTime }) {
-  const [timerStarted, setTimerStarted] = useState(false);
+  const [timerRemaining, setTimerRemaining] = useState(targetTime * 1000);
+  const timerIsActive =
+    timerRemaining > 0 && timerRemaining < targetTime * 1000;
 
   const timer = useRef(); //use UseRef to store value for component behind the scene so that value dont get lost when component reRenders.
   const dialogRef = useRef();
-  
+
+  if (timerRemaining <= 0) {
+    clearInterval(timer.current);
+    dialogRef.current.open();
+  }
   function handleStart() {
-    setTimerStarted(true);
-
-    timer.current = setTimeout(() => {
-      dialogRef.current.open();
-
-    }, targetTime * 1000);
+    timer.current = setInterval(() => {
+      setTimerRemaining((previousTime) => previousTime - 10);
+    }, 10);
   }
 
   function handleStop() {
-    clearTimeout(timer.current);
+    clearInterval(timer.current);
+    dialogRef.current.open();
   }
+
+  function resetTimer() {
+    setTimerRemaining(targetTime * 1000);
+  }
+
   return (
     <>
-    <ResultModal ref={dialogRef} targetTime={targetTime} result="Lost" />
+      <ResultModal
+        ref={dialogRef}
+        targetTime={targetTime}
+        timeRemaining={timerRemaining}
+        onReset={resetTimer}
+      />
       <div className="challenge">
         <h2>{title}</h2>
         <p className="challenge-time">
           {targetTime} {targetTime > 1 ? "Seconds" : "Second"}
         </p>
         <p>
-          <button onClick={timerStarted ? handleStop : handleStart}>
-            {timerStarted ? "Stop" : "Start"} Challenge
+          <button onClick={timerIsActive ? handleStop : handleStart}>
+            {timerIsActive ? "Stop" : "Start"} Challenge
           </button>
         </p>
-        <p className={timerStarted ? "active" : undefined}>
-          {timerStarted ? "Time is running..." : "inactive"}
+        <p className={timerIsActive ? "active" : undefined}>
+          {timerIsActive ? "Time is running..." : "inactive"}
         </p>
       </div>
     </>
